6f17d15cff6a8628d096e347772b2f34c0e12db7
Your Name <you@example.com>
2019-07-17 18:44:53 -0400

    created 2 buttons to filter the date

--- src/Components/PhotoGrid.js
+++ src/Components/PhotoGrid.js
@@ -4,20 +4,23 @@ import PhotoCard from "./PhotoCard";
 
 function PhotoGrid(){
     const [photos, setPhotos] = useState([]);
+    const [date, setDate] = useState("2012-02-14")
     
     useEffect(()=>{
         axios 
-            .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-02-14`)
+            .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
             .then(response => {
                 console.log('response', response)
                 const planet = response.data.url;
                 setPhotos(planet)
             })
-    },[]);
+    },[date]);
 
 
     return (
         <div className = "photo-grid">
+        <button onClick={()=>setDate('2012-02-14')}>2012-02-14</button>
+        <button onClick={()=>setDate('2012-02-15')}>2012-02-15</button>
         <PhotoCard key = {setPhotos} imgUrl = {photos}/>
         </div>
     )
